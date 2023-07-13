d44318d80ce556ceb64d697ffab9547bc47cf441
sawwalakhepallavi <sawwalakhepallavi@gmail.com>
2023-07-13 18:06:43 +0530

    using jpa read data from database

---
+++ spring-JPA/src/main/java/com/example/springJPA/service/GetStudentRecord.java
@@ -0,0 +1,23 @@
+package com.example.springJPA.service;
+
+import com.example.springJPA.model.Student;
+import com.example.springJPA.repo.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class GetStudentRecord {
+   @Autowired
+    private StudentRepo studentRepo;
+
+
+    public List<Student> getStudents(String name) {
+        return studentRepo.findByName(name);
+
+    }
+
+}
